50cbc18cbfed8c975672e02910b2d5acb1798478
Jude Antony <judebantony@gmail.com>
2021-11-19 14:07:29 +0530

    JUDE-35 updated teh tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,18 @@
 terraform {
+  
+  required_providers {
+   aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  
+  required_version = ">= 1.0.11"
+  
   backend "remote" {
     organization = "jba"
 
