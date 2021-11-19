43a1f01b80894126258cecd8be321768e4bdee1d
Jude Antony <judebantony@gmail.com>
2021-11-19 14:18:27 +0530

    JUDE-35 updated teh format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,5 @@
 terraform {
   
-  required_providers {
-   aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  
-  required_version = ">= 1.0.11"
-  
   backend "remote" {
     organization = "jba"
 
