8bca78cf7b36a8509f48b7938ca4f14fda0ec99f
Jude Antony <judebantony@gmail.com>
2021-11-20 22:58:30 +0530

    JUDE-36 fixed ts file

--- k6-test.ts
+++ k6-test.ts
@@ -1,17 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-export let options = {
+export const options = {
   ext: {
     loadimpact: {
       projectID: 3560884,
       // Test runs with the same name groups test runs together
       name: "Jude Test"
     }
-  }
-}
-
-export const options = {
+  },
+  	
   duration: '1m',
   vus: 50,
   thresholds: {
