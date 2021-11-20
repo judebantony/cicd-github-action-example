96f7338e969f5a19a0da03430ed833f7d60a2e62
Jude Antony <judebantony@gmail.com>
2021-11-20 22:00:23 +0530

    JUDE-36 test.ts

---
+++ test.ts
@@ -0,0 +1,25 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+  ext: {
+    loadimpact: {
+      projectID: 3560884,
+      // Test runs with the same name groups test runs together
+      name: "Jude Test"
+    }
+  }
+}
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+  thresholds: {
+    http_req_duration: ['p(95)<500'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://githubaction-terraform-example-2141246354.ap-south-1.elb.amazonaws.com');
+  sleep(1);
+}
