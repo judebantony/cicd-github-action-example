51b7151f2b1829172105ce492c367382c2bfef02
Jude Antony <judebantony@gmail.com>
2022-07-07 16:54:38 +0530

    JUDE-82 commiting to test the plugin

--- src/main/java/com/jba/ci/CIGitHubActionApplication.java
+++ src/main/java/com/jba/ci/CIGitHubActionApplication.java
@@ -14,5 +14,30 @@ public class CIGitHubActionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CIGitHubActionApplication.class, args);
 	}
-
+	
+	/**
+	 * 
+	 */
+	void compareNumber() {
+		int a = 10;
+		int b = 20;
+		if (a > b) {
+			System.out.println("a is greater than b");
+		} else if (a < b) {
+			System.out.println("a is less than b");
+		} else {
+			System.out.println("a is equal to b");
+		}
+	}
+	void compareNumber2() {
+		int a = 10;
+		int b = 20;
+		if (a > b) {
+			System.out.println("a is greater than b");
+		} else if (a < b) {
+			System.out.println("a is less than b");
+		} else {
+			System.out.println("a is equal to b");
+		}
+	}
 }
